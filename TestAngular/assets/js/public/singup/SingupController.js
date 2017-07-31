5ff047e286466d17f2fd93d519525c379ca79b89
upam <upam>
2017-07-31 13:25:32 -0500

    controller and modules
    controller and modules

---
+++ TestAngular/assets/js/public/singup/SingupController.js
@@ -0,0 +1,13 @@
+angular.module('SingupModule.controllers',[ ]).controller('SingupController',['$scope',function($scope){
+	$scope.user={
+		name:'MOVILES'
+	}
+
+}]);
+
+/*
+var myApp = angular.module('myApp',[]);∫
+
+myApp.controller('GreetingController', ['$scope', function($scope) {
+  $scope.greeting = 'Hola!';
+}]);*/
